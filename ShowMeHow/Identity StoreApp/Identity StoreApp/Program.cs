e6ee1b08cd8e68af4319f3273a9b2dbf3370b4e0
quyet nguyen van <7784738-quyetnguyen02@users.noreply.gitlab.com>
2021-08-08 15:12:02 +0700

    add

---
+++ ShowMeHow/Identity StoreApp/Identity StoreApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Identity_StoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the names to be concatenated");
+            Console.WriteLine("Enter first name");
+            string firstText;
+            firstText = Console.ReadLine();
+            Console.WriteLine("Enter second name");
+            string secoundText;
+            secoundText = Console.ReadLine();
+            string concatenatedString = firstText + " " + secoundText;
+            Console.WriteLine(concatenatedString);
+        }
+    }
+}
