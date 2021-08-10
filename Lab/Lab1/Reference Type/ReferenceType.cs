ace3aaf40f54431fb74fb021ac24ae08e80d71b7
quyet nguyen van <7784738-quyetnguyen02@users.noreply.gitlab.com>
2021-08-10 12:01:12 +0700

    add

---
+++ Lab/Lab1/Reference Type/ReferenceType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reference_Type
+{
+    
+    class ReferenceType
+    {
+        public int valueVal;
+    }
+    class TestReference
+    {
+        static void Main(string[] args)
+        {
+            ReferenceType refer = new ReferenceType();
+            refer.valueVal = 5;
+            Test(refer);
+            Console.WriteLine("The value of the variable is {0}", refer.valueVal);
+            Console.ReadLine();
+        }
+        static void Test(ReferenceType refer)
+        {
+            int temp = 5;
+            refer.valueVal = temp * 2;
+        }
+    }
+}
