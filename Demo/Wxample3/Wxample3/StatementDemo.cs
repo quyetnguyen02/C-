e173afb2b0123c0f6ea48256ddd6a2f0f5421c5a
quyet nguyen van <7784738-quyetnguyen02@users.noreply.gitlab.com>
2021-08-12 10:30:15 +0700

    add

---
+++ Demo/Wxample3/Wxample3/StatementDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wxample3
+{
+    class StatementDemo
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 5;
+            b = 6;
+            c = 7;
+            if(a<=b && b >= c)
+            {
+                Console.WriteLine("Ds da duoc sap xep tang dan");
+            }
+            else if(a>=b && b >= c)
+            {
+                Console.WriteLine("Ds sap xep giam dan");
+
+            }
+            else
+            {
+                Console.WriteLine("Ds can sap xep lai");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
