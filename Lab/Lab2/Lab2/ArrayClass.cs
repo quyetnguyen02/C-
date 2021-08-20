47bc4fc244ca6dbd723fecf2f76bde7ef5067701
quyet nguyen van <7784738-quyetnguyen02@users.noreply.gitlab.com>
2021-08-20 09:14:19 +0700

    add

--- Lab/Lab2/Lab2/ArrayClass.cs
+++ Lab/Lab2/Lab2/ArrayClass.cs
@@ -17,8 +17,31 @@ namespace Lab2
             int x = 0;
             while((x=Array.IndexOf(Arr,42,x)) >= 0)
             {
-
+                Console.WriteLine("42 found at index" + x);
+                ++x;
             }
+            x = Arr.Length - 1;
+
+            while ((x = Array.LastIndexOf(Arr, 42, x)) >= 0)
+            {
+                Console.WriteLine("42 found at index" + x);
+                --x;
+            }
+
+            Console.WriteLine("Array that before sorted");
+            for(int i = 0; i < Arr.Length; i++)
+            {
+                Console.WriteLine("{0}:  {1}", i + 1, Arr[i]);
+            }
+
+            Array.Reverse(Arr);
+            Console.WriteLine("Array that after reserse");
+            for(int i = 0; i < Arr.Length; i++)
+            {
+                Console.WriteLine("{0}:   {1}", i + 1, Arr[i]);
+            }
+
+            Console.ReadLine();
 
 
         }
