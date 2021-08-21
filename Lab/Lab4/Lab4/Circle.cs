6164c2286517fa935e1cc17b40af45c5e394ee1b
quyet nguyen van <7784738-quyetnguyen02@users.noreply.gitlab.com>
2021-08-21 08:44:20 +0700

    add

---
+++ Lab/Lab4/Lab4/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+   public class Circle : GeomatricObject
+    {
+        private double radius;
+        public Circle(double x)
+        {
+            this.radius = x;
+        }
+
+        public Circle(double x,string c,double w) : base(c, w)
+        {
+            this.radius = x;
+        }
+
+        public override string ToString()
+        {
+            return "Circle has:radius" + radius + ",color is" + PColor +"Weight is" + PWeight;
+        }
+
+        public override double findArea()
+        {
+            return Math.PI*radius*radius;
+        }
+
+        public override double findPerimeter()
+        {
+            return 2* Math.PI * radius;
+        }
+    }
+    
+    }
+
---
+++ Lab/Lab4/Lab4/GeomatricObject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public abstract class GeomatricObject
+    {
+        protected string color;
+        private double weight;
+
+        protected GeomatricObject()
+        {
+            color = "white";
+            weight = 1.0;
+        }
+
+        protected GeomatricObject(string color, double weight)
+        {
+            this.color = color;
+            this.weight = weight;
+        }
+
+        public string PColor
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public double PWeight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public abstract double findArea();
+
+        public abstract double findPerimeter();
+    }
+}
