e1e8fa61006b4c454e56bd8d67b4b9f9241f4c5e
quyet nguyen van <7784738-quyetnguyen02@users.noreply.gitlab.com>
2021-08-22 14:53:11 +0700

    add

---
+++ Lab/Customer/ExHandling1/ExHandling1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExHandling1
+{
+    class ExHandling1
+    {
+        static void Main(string[] args)
+        {
+            byte[] a = new byte[5];
+            //nhap mang
+
+            try
+            {
+                for(int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine("a[{0}]=", i + 1);
+                    a[i] =Convert.ToByte(Console.ReadLine());
+                }
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Khong duoc nhap ky tu cho mang so");
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Khong duoc nhap gia tri nam ngoai mien 0-255");
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Loi vuot qua pham vi cua mang");
+            }
+
+
+            //in mang
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("{0}", a[i]);
+            }
+
+        }
+    }
+}
---
+++ Lab/Customer/ExHandling2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExHandling2
+{
+    public class InvalidInput : ApplicationException
+    {
+        public InvalidInput() : base("Enter a number greater than zero") { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intCnt;
+            int intNum = 0;
+            Console.WriteLine("Enter anumber");
+            try
+            {
+                intNum = Convert.ToInt32(Console.ReadLine());
+                if(intNum <= 0)
+                {
+                    throw new InvalidInput();
+                }
+            }
+            catch(InvalidInput objInvalodInput)
+            {
+                Console.WriteLine(objInvalodInput.Message);
+            }
+            catch(System.FormatException objFormatException)
+            {
+                Console.WriteLine(objFormatException.Message);
+            }
+            finally
+            {
+                if (intNum > 0)
+                {
+                    for(intCnt = 1; intCnt <= 10; intCnt++)
+                    {
+                        Console.WriteLine(intCnt * intNum);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
