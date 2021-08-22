be4e515d99ff84e745aa2d6765a6f92a45a463c3
quyet nguyen van <7784738-quyetnguyen02@users.noreply.gitlab.com>
2021-08-22 15:18:23 +0700

    add

---
+++ Lab/Customer/‘FinallyStmt’/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _FinallyStmt_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream outStream = null;
+            FileStream inStream = null;
+            try
+            {
+                //mo file de ghi du lieu
+                outStream = File.OpenWrite("DestinationFile.txt");
+                //mo file de doc du lieu
+                inStream = File.OpenRead("BogusInputFile.txt");
+                //cac cau lenh doc du lieu tu file
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if(outStream != null)
+                {
+                    outStream.Close();
+                    Console.WriteLine("outStream closed.");
+                }
+                if(inStream != null)
+                {
+                    inStream.Close();
+                    Console.WriteLine("inStream closed.");
+                }
+            }
+        }
+    }
+}
