4fac749bc2796bc1348f10865a307d2ba79e549b
quyet nguyen van <7784738-quyetnguyen02@users.noreply.gitlab.com>
2021-08-22 15:09:53 +0700

    add

---
+++ Lab/Customer/‘ExHandling3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _ExHandling3
+{
+    class Program
+    {
+        public static int AnExceptionFunction(int value)
+        {
+            int x;
+            if(value == 0)
+            {
+                throw new DivideByZeroException("Divide By 0 error!");}
+                x = 20 / value;
+
+                return x;
+            
+        }
+        static void Main(string[] args)
+        {
+            int value = 0;
+            try
+            {
+                value = AnExceptionFunction(10);
+                    Console.WriteLine("Value = {0}", value);
+                  value=  AnExceptionFunction(0);
+                    Console.WriteLine("Value = {0}", value);
+                
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Caught an exception {0}.Contiuning", ex);
+            }
+            Console.WriteLine("Done");
+        }
+
+    }
+}
